403296dafc7ffa9fd3072e3b34b624d2d5d19cb0
Ava Barron <mztriz@gmail.com>
2018-09-14 19:37:39 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+    return
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+    return
+  })
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if (key.which ==)
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
 });
