8e78cc802b44658677fb574d161c3c41c60d4462
Ava Barron <mztriz@gmail.com>
2018-09-14 19:45:53 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,5 +21,12 @@ function pressIt(){
   })
 }
 
+function submitIt(){
+  $('form').on('submit', function (){
+    alert('your form is going to be submitted now.')
+    return
+  })
+}
+
 $(document).ready(function(){
 });
